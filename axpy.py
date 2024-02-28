33f6bf34a4675f5e98f3d48d24e6d53ba060e10a
Clark Fitzgerald <clarkfitzg@gmail.com>
2024-02-28 06:39:46 -0800

    .

---
+++ axpy.py
@@ -0,0 +1,19 @@
+
+def add1(x):
+    return x + 1
+
+a = 10
+b = add1(a)
+
+# What's the value of b?
+
+
+def add1(x):
+    """
+    Add 1 to x  (docstring)
+    """
+    # Notes for someone reading the code  (comment)
+    return x + 1
+
+
+def 
